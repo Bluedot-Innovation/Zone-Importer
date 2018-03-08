65f816572e84e6d08ecb58d7b76a3bb2738c8321
Richard Tan <richard@bluedotinnovation.com>
2018-03-08 12:53:24 +1100

    Polygon example working

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-exports.ZONES_POST_ENDPOINT = 'https://api.bluedotinnovation.com/1/zones';
+// exports.ZONES_POST_ENDPOINT = 'https://api.bluedotinnovation.com/1/zones';
+exports.ZONES_POST_ENDPOINT = 'https://apiuat3.bluedotinnovation.com/1/zones';
