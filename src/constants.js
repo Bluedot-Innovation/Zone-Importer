f9920341194847eaa80c441c9994461223c8c4fb
Richard Tan <richard@bluedotinnovation.com>
2018-03-08 16:52:13 +1100

    Update

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
-// exports.ZONES_POST_ENDPOINT = 'https://api.bluedotinnovation.com/1/zones';
-exports.ZONES_POST_ENDPOINT = 'https://apiuat3.bluedotinnovation.com/1/zones';
+exports.ZONES_POST_ENDPOINT = 'https://api.bluedotinnovation.com/1/zones';
